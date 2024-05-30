6a6456e5fab593fe99a0c7eaf6962f7e75440c7e
Thiago Campos <thiago.campos715@gmail.com>
2024-05-30 18:28:40 -0300

    Estudo sobre Escopo do código em Java

---
+++ Escopo/README.md
@@ -0,0 +1,2 @@
+## Escopo
+Vendo na prática como o escopo do código, lugar onde as variáveis foram declaradas, funcionam em uma classe e método
---
+++ Escopo/src/edu/thiago/escopo/Escopo.java
@@ -0,0 +1,18 @@
+package edu.thiago.escopo;
+
+public class Escopo {
+    public class Conta {
+        double saldo=10.0;
+        
+        public void sacar(Double valor) { //essa variavel valor só existe nesse método
+            double novoSaldo = saldo - valor;
+        }
+        public void imprimirSaldo(){
+            System.out.println(saldo); /*saldo vai estar disponível em todo o código pois foi 
+            declarada como uma variavel da classe*/
+            
+            System.out.println(/*novoSaldo*/);/*aqui novoSaldo vai gerar um erro, pois é uma variavel que foi
+            gerada diretamente no método anterior, ficando inacessível fora dele */
+        }
+    } 
+}
