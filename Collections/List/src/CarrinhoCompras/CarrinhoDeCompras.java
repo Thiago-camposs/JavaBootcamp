fba15e1431be75002f62cb744922aced9f9f573a
Thiago Campos <thiago.campos715@gmail.com>
2024-06-13 16:02:24 -0300

    Projeto de carrinho de compras com collection

---
+++ Collections/List/src/CarrinhoCompras/CarrinhoDeCompras.java
@@ -0,0 +1,38 @@
+package CarrinhoCompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CarrinhoDeCompras {
+    private List<Item> itemList;
+    public CarrinhoDeCompras(){
+        this.itemList = new ArrayList<>();
+    }
+    //Métodos
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itemList.add(new Item(nome,preco,quantidade));
+    }
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for (Item i : itemList) {
+            if (i.getNome().equalsIgnoreCase(nome)) {
+                itensParaRemover.add(i);
+            }
+        }
+        itemList.removeAll(itensParaRemover);
+    }
+    public double calcularValorTotal(){
+        double valorTotal= 0.0;
+        for (Item item : itemList) {
+            valorTotal += item.getPreco()*item.getQuantidade();
+        }
+        return valorTotal;
+    }
+    public void exibirItens(){
+        System.out.println("O total de itens no carrinho é de: ");
+        for (Item item : itemList) {
+            System.out.println("Produto: "+item.getNome()+"\nPreço:"+item.getPreco()+"\nQuantidade:"+item.getQuantidade()+"\n");
+        }
+    }
+}
---
+++ Collections/List/src/CarrinhoCompras/Main.java
@@ -0,0 +1,14 @@
+package CarrinhoCompras;
+
+public class Main {
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Caneta", 2, 10);
+        carrinhoDeCompras.adicionarItem("Borracha", 1, 2);
+        carrinhoDeCompras.adicionarItem("Lapiseira", 7.50, 1);
+        carrinhoDeCompras.removerItem("lapiseira");//como está aplicado ignore case vai funcionar
+        carrinhoDeCompras.exibirItens();
+        System.out.println("Valor total da compra: "+carrinhoDeCompras.calcularValorTotal());
+    }
+}
