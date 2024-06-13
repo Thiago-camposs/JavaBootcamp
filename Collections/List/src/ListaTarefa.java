ca369a2edd376346953fcdca7376150d24181171
Thiago Campos <thiago.campos715@gmail.com>
2024-06-13 14:45:06 -0300

    Projeto Lista de Tarefas usando collections

---
+++ Collections/List/src/ListaTarefa.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+//Construtor
+    public ListaTarefa(){
+        this.tarefaList = new ArrayList<>();
+    }
+//Métodos
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+}
---
+++ Collections/List/src/Tarefa.java
@@ -0,0 +1,14 @@
+public class Tarefa {
+    private String descricao;
+    //construtor
+    public Tarefa(String descricao){
+        this.descricao=descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    @Override
+    public String toString() {
+        return "Descricao: "+ descricao;
+    }
+}
